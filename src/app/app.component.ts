a9994b919fce651285b2749ff7f6f3c7ba6859be
Jeandro Alves Correia <jeandroac@gmail.com>
2022-09-04 09:19:41 -0300

    fix required

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'vinil-collection';
+  isActive = true;
+  isAdmin = true;
+  coords: any;
+  currentDate: Date;
+
+  constructor() {
+    this.currentDate = new Date();
+  }
+
+  ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.coords = position.coords;
+    });
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  }
 }
+
